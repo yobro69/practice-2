5c50c5e7e2cf5c6df055f8dce56261e60dd7e469
yobro69 <92798702+yobro69@users.noreply.github.com>
2021-10-21 09:02:33 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(30)
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P0.setAngle(180)
+})
+basic.forever(function () {
+	
+})
